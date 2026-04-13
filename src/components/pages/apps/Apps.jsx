0da369407f2cb986a6da938b6ef0bd347fe80dfb
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-13 21:27:32 +0600

    Enhance Apps component with improved layout and loading state handling; add descriptive header

--- src/components/pages/apps/Apps.jsx
+++ src/components/pages/apps/Apps.jsx
@@ -18,19 +18,28 @@ const Apps = () => {
     };
     fetchData();
   }, []);
+
   console.log("Fetching data...", apps, loading);
 
   return (
-    <div className=" container mx-auto my-20">
+    <div className="container mx-auto my-10">
+      <div className="mt-8 mb-8 text-center max-w-[50%] mx-auto">
+        <h2 className="font-bold text-4xl">All apps</h2>
+        <p className=" text-gray-600">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          reiciendis eos laborum tempore quis hic quod cupiditate, consequuntur
+        </p>
+      </div>
+
       {loading ? (
-        <div className="flex justify-center items-center h-64">
+        <div className="flex justify-center items-center">
           <HashLoader color="#ad46ff" />
         </div>
       ) : (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {apps.map((app) => (
-            <AppCard app={app} key={app.id} />
-          ))}
+        <div className=" grid sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-5">
+          {apps.map((app, ind) => {
+            return <AppCard app={app} key={ind} />;
+          })}
         </div>
       )}
     </div>
