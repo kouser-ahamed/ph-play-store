72fa4ed15675a4e593072c75e83deb997f19ce42
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-13 14:58:48 +0600

    Refactor Navbar to use a dynamic array for navigation items for improved reusability

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -5,21 +5,46 @@ import { FaGithub } from "react-icons/fa";
 import MyNavLink from "./MyNavLink";
 
 const Navbar = () => {
+  // This is the new code, I have added this array to make the navbar more dynamic and reusable.
+  const navItems = [
+    {
+      path: "/",
+      text: "Home",
+    },
+
+    {
+      path: "/apps",
+      text: "Apps",
+    },
+    {
+      path: "/installedApps",
+      text: "Installation",
+    },
+  ];
+
   return (
     <div>
       <nav className="shadow">
         <div className="flex justify-between gap-4 items-center py-[8px] container mx-auto">
           <img src={logoImg} alt="LogoImage" className="w-[50px]" />
           <ul className="flex justify-between items-center gap-4">
-            <li>
+           
+            {/* This is the new code, I have replaced the old code with this to make it more dynamic and reusable. */}
+
+            {navItems.map((item) => (
+              <MyNavLink to={item.path}>{item.text}</MyNavLink>
+            ))}
+
+            {/* 
+            This is the old code, I have replaced it with the above code to make it more dynamic and reusable. */}
+
+            {/* <li>
               <MyNavLink to="/">Home</MyNavLink>
             </li>
             <li>
-              <MyNavLink to="/apps">Apps</MyNavLink>
-            </li>
-            <li>
               <MyNavLink to="/installedApps">Installation</MyNavLink>
-            </li>
+            </li> */}
+            
           </ul>
           <button className="btn bg-purple-500 text-white rounded-xl">
             <FaGithub />
