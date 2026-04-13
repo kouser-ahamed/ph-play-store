edc55f8cdfda0696f40ffce749a3353d2e998bcc
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-13 11:02:28 +0600

    Add React Router and create RootLayout component for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router/dom";
+import { createBrowserRouter } from "react-router";
+import RootLayout from "./layout/RootLayout";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        path: "/",
+        element: <h2>HomePage</h2>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h2 className='bg-amber-500'>Hello</h2>
-    
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
