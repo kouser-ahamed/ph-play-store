880c2de884ca1860f6dc4f85ebbedaa723e2af7c
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-13 11:19:03 +0600

    Add Navbar and Footer components to RootLayout

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router";
+import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
 
 const RootLayout = () => {
   return (
     <div>
-      <h2> Navbar</h2>
-      <Outlet> </Outlet>
-      <h2>Footer</h2>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
